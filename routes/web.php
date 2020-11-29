9f05b6a026383902c25b6735d3e15ec695a21e9a
HSIAO,KAI-FU <Tokizakimadthree@gmail.com>
2020-11-29 14:17:09 +0800

    修正:改用HomeController，卻未更改route去串接HomeController

--- routes/web.php
+++ routes/web.php
@@ -14,19 +14,22 @@ use App\Http\Controllers\HelloController;
 |
 */
 
-/*Route::get('/', function () {
+Route::get('/', function () {
     return view('welcome');
 });
-*/
+
 /*
 Route::get('home',function(){
     return view('home');
 });
 */
-/*
-Route::get('home',function(){
-    return view('home.index');
-});
-*/
+
+
+//Route::get('home',function(){
+//    return view('home.index');
+//});
+
+Route::get('home',[HomeController::class, 'index'])->name('home.index');
+
 Route::get('hello/{name?}', [HelloController::class, 'index'])->name('hello.index');
 
